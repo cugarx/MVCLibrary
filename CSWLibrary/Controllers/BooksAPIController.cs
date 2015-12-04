fe471092a074bdf9fca640a7de8866d08c7cb496
cugarx <cugar_x@hotmail.com>
2015-12-04 12:00:49 -0600

    First check in

---
+++ CSWLibrary/Controllers/BooksAPIController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CSWLibrary;
+using System.Web.Script.Serialization;
+
+namespace CSWLibrary.Controllers
+{
+    public class BooksAPIController : ApiController
+    {
+        private LibraryEntities db = new LibraryEntities();
+
+        // GET: api/BooksAPI
+        public IQueryable<Book> GetBook()
+        {
+            return db.Book;
+        }
+
+        // GET: api/BooksAPI/5
+        [ResponseType(typeof(Book))]
+        public async Task<IHttpActionResult> GetBook(int id)
+        {
+            Book book = await db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        // PUT: api/BooksAPI/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutBook(int id, Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/BooksAPI
+        [ResponseType(typeof(Book))]
+        public async Task<IHttpActionResult> PostBook(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Book.Add(book);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = book.Id }, book);
+        }
+
+        // DELETE: api/BooksAPI/5
+        [ResponseType(typeof(Book))]
+        public async Task<IHttpActionResult> DeleteBook(int id)
+        {
+            Book book = await db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            db.Book.Remove(book);
+            await db.SaveChangesAsync();
+
+            return Ok(book);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BookExists(int id)
+        {
+            return db.Book.Count(e => e.Id == id) > 0;
+        }
+
+        // GET: api/BooksAPI/5
+        public string GetBooks()
+        {
+            List<Book> books = (from u in db.Book select u).ToList();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            return js.Serialize(books);
+        }
+
+
+        [ResponseType(typeof(List<Book>))]
+        public async Task<List<Book>> GetBookByAuthor(int idAuthor)
+        {
+            return  await db.Book.Where(c => c.IdAuthor == idAuthor).ToListAsync();
+        }
+
+        [ResponseType(typeof(List<Book>))]
+        public async Task<IHttpActionResult> GetBookByCategory(int idCategory)
+        {
+            List<Book> books = await db.Book.Where(c => c.IdCategory == idCategory).ToListAsync();
+            return Ok(books);
+        }
+    }
+}
---
+++ CSWLibrary/Controllers/BooksController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CSWLibrary;
+
+namespace CSWLibrary.Controllers
+{
+    public class BooksController : Controller
+    {
+        private LibraryEntities db = new LibraryEntities();
+
+        // GET: Books
+        public async Task<ActionResult> Index()
+        {
+            int idAuthor = 0;
+            var book = db.Book.Include(b => b.Author).Include(b => b.Category);
+
+            if (!String.IsNullOrEmpty(Request.QueryString["idAuthor"]))
+            {
+                idAuthor = Int32.Parse(Request.QueryString["idAuthor"]);
+                book = db.Book.Include(b => b.Author).Include(b => b.Category).Where(b => b.IdAuthor == idAuthor);
+                ViewBag.Autores = new SelectList(db.Author, "Id", "Name", idAuthor);
+            }
+            else
+                ViewBag.Autores = new SelectList(db.Author, "Id", "Name", 0);
+            
+            return View(await book.ToListAsync());
+        }
+
+        // GET: Books/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = await db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // GET: Books/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdAuthor = new SelectList(db.Author, "Id", "Name");
+            ViewBag.IdCategory = new SelectList(db.Category, "Id", "Name");
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,IdAuthor,IdCategory,ISBN,Title,Publisher,Description")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Book.Add(book);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdAuthor = new SelectList(db.Author, "Id", "Name", book.IdAuthor);
+            ViewBag.IdCategory = new SelectList(db.Category, "Id", "Name", book.IdCategory);
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = await db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdAuthor = new SelectList(db.Author, "Id", "Name", book.IdAuthor);
+            ViewBag.IdCategory = new SelectList(db.Category, "Id", "Name", book.IdCategory);
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,IdAuthor,IdCategory,ISBN,Title,Publisher,Description")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(book).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdAuthor = new SelectList(db.Author, "Id", "Name", book.IdAuthor);
+            ViewBag.IdCategory = new SelectList(db.Category, "Id", "Name", book.IdCategory);
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = await db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Book book = await db.Book.FindAsync(id);
+            db.Book.Remove(book);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Search(string searchString)
+        {
+            var books = from m in db.Book
+                             select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(s => s.Author.Name.Contains(searchString));
+            }
+
+            return View(books);
+        }
+    }
+}
